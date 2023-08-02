d610b427eee260bcb1530a74fb756ba7c1fad6fc
mcxufly <mcxufly@outlook.com>
2023-08-02 13:14:14 +0800

    TODO

--- src/main/java/dev/positivee/duanwure/item/ItemZongZi.java
+++ src/main/java/dev/positivee/duanwure/item/ItemZongZi.java
@@ -1,11 +1,24 @@
 package dev.positivee.duanwure.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ItemZongZi extends Item
 {
 	public ItemZongZi(Properties pProperties)
 	{
 		super(pProperties);
+	}
+
+	@Override
+	public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced)
+	{//TODO:添加展示食用效果的tooltip
+		super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
 	}
 }
